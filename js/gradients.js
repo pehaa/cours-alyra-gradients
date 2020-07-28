85254e75d363ea5eaabbf3f3fb6aedb5135d505a
PeHaa <info@pehaa.com>
2020-07-28 08:35:55 +0200

    current progress

--- js/gradients.js
+++ js/gradients.js
@@ -150,3 +150,23 @@ const gradients = [
     tags: ["vert", "jaune"],
   },
 ]
+
+function allTags(list) {
+  /* retourner la liste des tags uniques */
+  let listTotal = []
+  for (let element of list) {
+    if ("tags" in element) {
+      listTotal = listTotal.concat(element.tags)
+    }
+  }
+  const listTagsUnique = []
+  listTotal.forEach((el) => {
+    if (!listTagsUnique.includes(el)) {
+      //listTagsUnique = listTagsUnique.concat([el])
+      listTagsUnique.push(el)
+    }
+  })
+  return listTagsUnique
+}
+
+const uniqueTags = allTags(gradients)
--- js/script.js
+++ js/script.js
@@ -8,18 +8,23 @@ pageHeader -> element du document avec l'id header
 switchGradientButton -> element du document avec l'id header-button
 tagFilter -> par quoi on filtre des gradients (par défaut "toutes les couleurs")
 */
+const introParagraph = document.getElementById("intro")
+console.log(uniqueTags)
 
 // functions
 
 // app
 /* inserer l'information : Voici une collection de ... dégradés... (1) */
 
+introParagraph.textContent = `Voici une collection de ${gradients.length} dégradés prêts à utiliser dans vos feuilles de styles
+`
 /* choisir un dégradé (random) et l'appliquer en tant que background image dans
 l'élément header (2a) */
 
+function generateHeader() {}
+generateHeader()
 /* click sur le bouton change le dégradé dans le header (2b) */
 
 /* inserer la grille des dégradés */
 
 /* activer le select by tag */
-
